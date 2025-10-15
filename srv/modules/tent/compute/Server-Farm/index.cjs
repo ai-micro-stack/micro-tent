59a03706058f0921cceee1b1d7f02ea47d0006db
big-data-consultant <big.data.consultant@outlook.com>
2025-10-15 10:50:14 -0400

    initial commit

--- srv/modules/tent/compute/Server-Farm/index.cjs
+++ srv/modules/tent/compute/Server-Farm/index.cjs
@@ -1,11 +1,12 @@
 const fs = require("fs");
-const { step1Ollma } = require("./step1_ollama");
-const { step2Service } = require("./step2_service");
+// const { step1Ollma } = require("./step1_ollama");
+// const { step2Service } = require("./step2_service");
 // const { step3Worker } = require("./step3_worker");
 
 async function ollama(confObj) {
-  await step1Ollma(confObj);
-  await step2Service(confObj);
+  console.log("Nothing need to install on Server Farm cluster...")
+  // await step1Ollma(confObj);
+  // await step2Service(confObj);
   // await step3Worker(confObj);
 }
 
